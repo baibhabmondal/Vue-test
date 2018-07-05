097a2a5c7c73623d76e0cf87bca985b8a66e282d
Baibhab Mondal <vbmandal98@gmail.com>
2018-07-05 15:29:13 +0530

    store modules created

---
+++ src/store/courseSchedule/index.js
@@ -0,0 +1,122 @@
+export default {
+  state: {
+    courseForSchedule: [
+      {
+        department: 'CSE',
+        courses: [
+          {
+            code: '12AD12',
+            name: 'Algorithms',
+            date: null,
+            session: null
+          },
+          {
+            code: '12AD12',
+            name: 'Algorithms',
+            date: null,
+            session: null
+          },
+          {
+            code: '12AD12',
+            name: 'Algorithms',
+            date: null,
+            session: null
+          },
+          {
+            code: '12AD12',
+            name: 'Algorithms',
+            date: null,
+            session: null
+          }
+        ]
+      },
+      {
+        department: 'IT',
+        courses: [
+          {
+            code: '12AD12',
+            name: 'Algorithms',
+            date: null,
+            session: null
+          },
+          {
+            code: '12AD12',
+            name: 'Algorithms',
+            date: null,
+            session: null
+          },
+          {
+            code: '12AD12',
+            name: 'Algorithms',
+            date: null,
+            session: null
+          },
+          {
+            code: '12AD12',
+            name: 'Algorithms',
+            date: null,
+            session: null
+          }
+        ]
+      },
+      {
+        department: 'ECE',
+        courses: [
+          {
+            code: '12AD12',
+            name: 'Algorithms',
+            date: null,
+            session: null
+          },
+          {
+            code: '12AD12',
+            name: 'Algorithms',
+            date: null,
+            session: null
+          },
+          {
+            code: '12AD12',
+            name: 'Algorithms',
+            date: null,
+            session: null
+          },
+          {
+            code: '12AD12',
+            name: 'Algorithms',
+            date: null,
+            session: null
+          }
+        ]
+      },
+      {
+        department: 'SWE',
+        courses: [
+          {
+            code: '12AD12',
+            name: 'Algorithms',
+            date: null,
+            session: null
+          },
+          {
+            code: '12AD12',
+            name: 'Algorithms',
+            date: null,
+            session: null
+          },
+          {
+            code: '12AD12',
+            name: 'Algorithms',
+            date: null,
+            session: null
+          },
+          {
+            code: '12AD12',
+            name: 'Algorithms',
+            date: null,
+            session: null
+          }
+        ]
+      }
+    ]
+  }
+}
